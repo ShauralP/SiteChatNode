6c86357b7993ecd663efb88dc35db6f4590ec066
Shaural Patel <Shaural Patel>
2017-01-22 02:15:58 -0500

    replaced wrong word from copying

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.post('/storedata', function (req, res) {
 
 var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 
